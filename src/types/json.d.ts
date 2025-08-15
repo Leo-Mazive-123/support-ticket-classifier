5fd70b7bd468191a9ebdbda17f8482271e5f87b7
Leo-Mazive-123 <leomazive00@gmail.com>
2025-08-15 12:32:06 +0200

    Project Working

--- src/types/json.d.ts
+++ src/types/json.d.ts
@@ -1,4 +1,6 @@
-declare module '*.json' {
-  const value: any;
+// src/types/json.d.ts
+
+declare module "*.json" {
+  const value: { [key: string]: unknown } | unknown[];
   export default value;
 }
